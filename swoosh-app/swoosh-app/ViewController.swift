3e55a291b300f7c3b093348da41b8d318c598f96
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2018-12-08 21:48:32 +0400

    First Steps

---
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh-app
+//
+//  Created by Sergey Neupokoev on 8/12/18.
+//  Copyright © 2018 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
